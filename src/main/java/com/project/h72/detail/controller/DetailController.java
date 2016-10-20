d367bc9cfbc00df586c0fa5036858a41351e7c16
0MyeongJun <0MyeongJun@DESKTOP-NU9D0BM>
2016-10-20 10:37:42 +0900

    디테일 컨트롤러 수정

--- src/main/java/com/project/h72/detail/controller/DetailController.java
+++ src/main/java/com/project/h72/detail/controller/DetailController.java
@@ -1,20 +1,19 @@
 package com.project.h72.detail.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.wappl.moolmool.board.controller.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DetailController {
 	/*@RequestMapping(value="/", method= RequestMethod.POST)*/
-	@RequsetMapping(value="/selectItem", method=RequestMethod.POST)
+	@RequestMapping(value = "/selectItem", method = RequestMethod.POST)
 	public String selectItem(String ItemDetailId, Model model){
 		
 		return "";
 	}
-	
-	@RequestMapping(value="/addReview", mtehod=RequestMethod.POST)
+	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
 	public String addReview(String ItemDetailId, Model model){
 		
 		return "";//ajax사용 출력
