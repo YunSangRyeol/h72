8d3302b828e6972652ea572a8da6e4269f5ed97e
euihyun <yoogoodcap@gmail.com>
2016-10-20 12:19:15 +0900

    qna vo

--- src/main/java/com/project/h72/notice/vo/Notice.java
+++ src/main/java/com/project/h72/notice/vo/Notice.java
@@ -3,10 +3,50 @@ package com.project.h72.notice.vo;
 import java.sql.Date;
 
 public class Notice {
+	public Notice(){}
 	
-	private int NOTICE_NO;
-	private String NOTICE_TITLE;
-	private String NOTICE_CONTENTS;
-	private Date ENROLLDATE;
+	private int noticeNo; //글번호
+	private String noticeTitle; //글제목
+	private String noticeContent; //글내용
+	private Date enrolldate; //날짜
+	private String noticeWriter; //작성자
+	
+	
+	
+	public Notice(int noticeNo, String noticeTitle, String noticeContent, Date enrolldate) {
+		super();
+		this.noticeNo = noticeNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.enrolldate = enrolldate;
+	}
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	public String getNoticeContent() {
+		return noticeContent;
+	}
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+	public Date getEnrolldate() {
+		return enrolldate;
+	}
+	public void setEnrolldate(Date enrolldate) {
+		this.enrolldate = enrolldate;
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/com/project/h72/qna/vo/QnA.java
+++ src/main/java/com/project/h72/qna/vo/QnA.java
@@ -1,5 +1,113 @@
 package com.project.h72.qna.vo;
 
+import java.sql.Date;
+
 public class QnA {
+
+	private int qNo; // 글 번호
+	private String qTitle; // 제목
+	private String qContent; // 내용
+	private Date qDate; // 작성일
+	private String qWriter; // 질문작성자
+	private int answer; // 질문 답변 여부
+	private String answerContent; // 답변내용
+	private Date aDate; // 답변일
+	private String qName; // 분류명
+	private int qCode; // 분류코드
+
+	public QnA(int qNo, String qTitle, String qContent, Date qDate, String qWriter, int answer, String answerContent,
+			Date aDate, String qName, int qCode) {
+		super();
+		this.qNo = qNo;
+		this.qTitle = qTitle;
+		this.qContent = qContent;
+		this.qDate = qDate;
+		this.qWriter = qWriter;
+		this.answer = answer;
+		this.answerContent = answerContent;
+		this.aDate = aDate;
+		this.qName = qName;
+		this.qCode = qCode;
+	}
+
+	public int getqNo() {
+		return qNo;
+	}
+
+	public void setqNo(int qNo) {
+		this.qNo = qNo;
+	}
+
+	public String getqTitle() {
+		return qTitle;
+	}
+
+	public void setqTitle(String qTitle) {
+		this.qTitle = qTitle;
+	}
+
+	public String getqContent() {
+		return qContent;
+	}
+
+	public void setqContent(String qContent) {
+		this.qContent = qContent;
+	}
+
+	public Date getqDate() {
+		return qDate;
+	}
+
+	public void setqDate(Date qDate) {
+		this.qDate = qDate;
+	}
+
+	public String getqWriter() {
+		return qWriter;
+	}
+
+	public void setqWriter(String qWriter) {
+		this.qWriter = qWriter;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+	public String getAnswerContent() {
+		return answerContent;
+	}
+
+	public void setAnswerContent(String answerContent) {
+		this.answerContent = answerContent;
+	}
+
+	public Date getaDate() {
+		return aDate;
+	}
+
+	public void setaDate(Date aDate) {
+		this.aDate = aDate;
+	}
+
+	public String getqName() {
+		return qName;
+	}
+
+	public void setqName(String qName) {
+		this.qName = qName;
+	}
+
+	public int getqCode() {
+		return qCode;
+	}
+
+	public void setqCode(int qCode) {
+		this.qCode = qCode;
+	}
 
 }
