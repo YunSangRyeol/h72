1bc1182a4e0b794b47d77d221c6a15926bacfe93
0MyeongJun <0MyeongJun@DESKTOP-NU9D0BM>
2016-10-20 11:15:10 +0900

    디테일 컨트롤러 insertCart 추가

--- src/main/java/com/project/h72/detail/controller/DetailController.java
+++ src/main/java/com/project/h72/detail/controller/DetailController.java
@@ -8,18 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DetailController {
 	/*@RequestMapping(value="/", method= RequestMethod.POST)*/
-	@RequestMapping(value = "/selectItem", method = RequestMethod.POST)
+	@RequestMapping(value = "/selectItem", method = RequestMethod.GET)
 	public String selectItem(String ItemDetailId, Model model){
 		
 		return "";
 	}
-	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
-	public String addReview(String ItemDetailId, Model model){
+	
+	@RequestMapping(value = "/insertReview", method = RequestMethod.GET)
+	public String insertReview(String ItemDetailId, Model model){
 		
 		return "";//ajax사용 출력
 	}
 	
-	
+	@RequestMapping(value = "/insertCart", method = RequestMethod.GET)
+	public String insertCart(String ItemId, Model model){
+		
+		return "";
+	}
 	
 	
 }
