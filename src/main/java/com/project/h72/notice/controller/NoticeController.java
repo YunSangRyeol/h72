3b9394e27d49eb3ae6104a83aaff11e72fa78ec5
euihyun <yoogoodcap@gmail.com>
2016-10-27 09:58:59 +0900

    notice qna controller

--- src/main/java/com/project/h72/notice/controller/NoticeController.java
+++ src/main/java/com/project/h72/notice/controller/NoticeController.java
@@ -4,26 +4,27 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.project.h72.HomeController;
-
+@Controller
 public class NoticeController {
 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/notice_list", method = RequestMethod.GET)
+	@RequestMapping(value = "boader/notice_list", method = RequestMethod.GET)
 	public String notice_list(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+/*		logger.info("Welcome home! The client locale is {}.", locale);
 
 		
-		model.addAttribute("serverTime" );
+		model.addAttribute("serverTime" );*/
 		
-		return "notice_list";
+		return "boader/notice_list";
 	}
 
 
@@ -31,27 +32,27 @@ private static final Logger logger = LoggerFactory.getLogger(HomeController.clas
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/notice_detail", method = RequestMethod.GET)
+	@RequestMapping(value = "boader/notice_detail", method = RequestMethod.GET)
 	public String notice_detail(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+/*		logger.info("Welcome home! The client locale is {}.", locale);
 
 		
-		model.addAttribute("serverTime" );
+		model.addAttribute("serverTime" );*/
 		
-		return "notice_detail";
+		return "boader/notice_detail";
 	}
 
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/notice_update", method = RequestMethod.GET)
+	@RequestMapping(value = "boader/notice_update", method = RequestMethod.GET)
 	public String notice_update(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+/*		logger.info("Welcome home! The client locale is {}.", locale);
 
 		
-		model.addAttribute("serverTime" );
+		model.addAttribute("serverTime" );*/
 		
-		return "notice_update";
+		return "boader/notice_update";
 	}
 }
--- src/main/java/com/project/h72/qna/controller/QnAController.java
+++ src/main/java/com/project/h72/qna/controller/QnAController.java
@@ -4,45 +4,46 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.project.h72.HomeController;
-
+@Controller
 public class QnAController {
 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-@RequestMapping(value = "/qna_list", method = RequestMethod.GET)
+@RequestMapping(value = "boader/qna_list", method = RequestMethod.GET)
 public String qna_list(Locale locale, Model model) {
-	logger.info("Welcome home! The client locale is {}.", locale);
+/*	logger.info("Welcome home! The client locale is {}.", locale);
 
 	
-	model.addAttribute("serverTime" );
+	model.addAttribute("serverTime" );*/
 	
-	return "qna_list";
+	return "boader/qna_list";
 }
 
-@RequestMapping(value = "/qna_detail", method = RequestMethod.GET)
+@RequestMapping(value = "boader/qna_detail", method = RequestMethod.GET)
 public String qna_detail(Locale locale, Model model) {
-	logger.info("Welcome home! The client locale is {}.", locale);
+/*	logger.info("Welcome home! The client locale is {}.", locale);
 
 	
-	model.addAttribute("serverTime" );
+	model.addAttribute("serverTime" );*/
 	
-	return "qna_detail";
+	return "boader/qna_detail";
 }
-@RequestMapping(value = "/qna_update", method = RequestMethod.GET)
+@RequestMapping(value = "boader/qna_update", method = RequestMethod.GET)
 public String qna_update(Locale locale, Model model) {
-	logger.info("Welcome home! The client locale is {}.", locale);
+/*	logger.info("Welcome home! The client locale is {}.", locale);
 
 	
-	model.addAttribute("serverTime" );
+	model.addAttribute("serverTime" );*/
 	
-	return "qna_update";
+	return "boader/qna_update";
 }
 
 }
